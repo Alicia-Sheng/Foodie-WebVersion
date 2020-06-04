c2de9c78591b185b2b6cf3dd8e4f48ead199b819
Wenxuan Jiang <wenxuanjiang@brandeis.edu>
2020-06-04 11:41:57 -0400

    added two items

--- assets/data.js
+++ assets/data.js
@@ -2,12 +2,20 @@ const data =
 [
   {
    "id": 1,
-    "name": "",
-    "desc": "",
-    "location": "",
-    "price": "",
-    "img": ""
-  }
+    "name": "Caesar Salad",
+    "desc": "Crisp Romaine lettuce and Garlic Croutons tossed in Creamy Caesar dressing with shaved Parmesan Cheese.",
+    "location": "The Stein",
+    "price": 6.69,
+    "img": "./media/CaesarSalad.jpg"
+  },
+  {
+    "id": 2,
+     "name": "Sausage, Egg & Cheese on a Croisssant",
+     "desc": "A tasty sandwich stacked with egg, American cheese and irresistible sausage.",
+     "location": "Dunkin' Donuts",
+     "price": 5.89,
+     "img": "./media/SausageEggCheese.jpg"
+   }
 ]
 
 export default data;
