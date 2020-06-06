327b0e19d4eb032a9eeb3ced6984c59f553d0bc0
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-06-05 22:34:57 -0400

    Add nav

--- src/index.js
+++ src/index.js
@@ -5,18 +5,40 @@ import CardList from './containers/CardList';
 
 import foodData from '../assets/data.js'
 
+import './style.css'
 
 const App = () => {
   return (
    <>
-    
+    <nav id="mainNavbar" className="navbar navbar-dark navbar-expand-md sticky-top">
+      <a href="#" className="navbar-brand">FOODIE</a>
+      <button className="navbar-toggler" data-toggle="collapse" data-target="#navLinks" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navLinks">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a href="" className="nav-link">HOME</a>
+          </li>
+          <li className="nav-item">
+            <a href="" className="nav-link">RESTAURANTS</a>
+          </li>
+          <li className="nav-item">
+            <a href="" className="nav-link">ABOUT</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <div className='container-fluid'>
-      <nav className='navbar navbar-light bg-white'>
-        <h1 className='navbar-brand text-black'>Featured Food</h1>
-      </nav>
+
+      <div className="title-wrapper text-center">
+        <div className="title-inner">
+          <h2 itemProp="headline">Choose & Enjoy</h2>
+        </div>
+      </div>
 
       <CardList data={foodData} />
-
 
     </div>
    </>
