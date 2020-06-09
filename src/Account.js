0ccddbfbd3bfd570e6bbd032297d6667b601421f
Wenxuan Jiang <wenxuanjiang@brandeis.edu>
2020-06-09 15:23:35 -0400

    Update login page

--- src/Account.js
+++ src/Account.js
@@ -6,12 +6,11 @@ import './style.css'
 const Account = () => {
   return (
     <div>
-
         <h1>This is the account page!</h1>
         <div>
-            <Link to="/account/login" class="link">Jump to Login Page</Link>
+            <Link to="/account/login" className="link">Jump to Login Page</Link>
             <br></br>
-            <Link to="/account/register" class="link">Jump to Register Page</Link>
+            <Link to="/account/register" className="link">Jump to Register Page</Link>
         </div>
 
     </div>
