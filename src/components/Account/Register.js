550a119b09ec350efec02613b3ebcc432b922cc4
Wenxuan Jiang <wenxuanjiang@brandeis.edu>
2020-06-09 17:56:56 -0400

    Update Register.js

--- src/components/Account/Register.js
+++ src/components/Account/Register.js
@@ -1,14 +1,28 @@
 import React from 'react';
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { Link } from "react-router-dom";
 import './Register.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Register() {
   return (
-    <div>
-      <h1>This is the register page!!</h1>
+    <div className='container-fluid'>
+      <div className='container'>
+        <div className='login-form'>
+          <h1>Create an account</h1>
+          <form>
+            <div className="form-group">
+              <input className="form-control" type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <input className="form-control" type="password" name="password" placeholder="password" />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-lg btn-primary btn-block">Sign Up</button>
+            </div>
+          </form>
+          <Link to="/account" className="link">Back to Account Page</Link>
+        </div>
+      </div>
     </div>
   )
 }
