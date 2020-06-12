2fa84b2fc730f0151035046ac559818231e087b7
MRSA-J <mrsa@brandeis.edu>
2020-06-12 11:01:40 +0800

    add restaurant pages

---
+++ src/components/Menu/Einstein.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import MenuList from '../../containers/MenuList'
+
+import foodData from '../../../assets/data'
+
+import '../../style.css'
+
+function Einstein() {
+  return (
+    <section>
+      <div className="block">
+        <div className="title-wrapper text-center">
+          <div className="title-inner">
+            <h2 itemProp="headline">Einstein</h2>
+          </div>
+        </div>
+        <MenuList data={foodData} dinHall="Einstein" />
+      </div>
+    </section>
+  )
+}
+
+export default Einstein
---
+++ src/components/Menu/Starbucks.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import MenuList from '../../containers/MenuList'
+
+import foodData from '../../../assets/data'
+
+import '../../style.css'
+
+function Starbucks() {
+  return (
+    <section>
+      <div className="block">
+        <div className="title-wrapper text-center">
+          <div className="title-inner">
+            <h2 itemProp="headline">Starbucks</h2>
+          </div>
+        </div>
+        <MenuList data={foodData} dinHall="Starbucks" />
+      </div>
+    </section>
+  )
+}
+
+export default Starbucks
