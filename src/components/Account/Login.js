10280d6fb96cdce127daa6a76c38c6b78869da7d
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-06-14 14:57:59 -0700

    Add local storage

--- src/components/Account/Login.js
+++ src/components/Account/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import './Login.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -21,7 +22,44 @@ class Login extends React.Component{
 
   // Temporary function
   handleSubmit(event){
+    let obj = [];
+    const username = this.user.value;
+    const password = this.password.value;
+    let user = localStorage.getItem('user');
+    let history = useHistory();
+    if (user) {
+      // if user exist
+      // compare locally stored value with input
+      let flag = obj.some(item => {
+        if (item.username === username) return true // username exists
+      })
+
+      if (flag) {// check password if username exists
+        // compare locally stored value with input
+        let f = false
+        obj.map(item => {
+          if (item.username === username && item.password === password) {
+            f = true;
+            return f;
+          }
+        })
+        if (f) {// user logged in if user credentials verified
+          alert('Wecome back, ' + username + "!")
+          history.push('/home')
+        } else {
+          alert('Wrong password!')
+        }
+
+      } else {// if username not found
+        alert('Username not found, plese sign up!')
+      }
+
+    } else {
+      // if user not found
+      alert('User not found, plese sign up!')
+    }
     alert('Username: ' + this.state.username + ' Password: ' + this.state.password);
+
     event.preventDefault();
   }
 
